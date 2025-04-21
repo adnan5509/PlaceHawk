0599a29ba277feed878b024919a08a38f66e9b0b
adnan5509 <adnanafzal5509@gmail.com>
2025-04-21 15:32:21 +0200

    implement HTTP client in AvailablePlacesComponent for fetching places data

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,29 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit, OnDestroy {
   places = signal<Place[] | undefined>(undefined);
+  subscriptions: Subscription[] = [];
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit() {
+    this.subscriptions.push(this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((response) => response.places),
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        }
+      }));
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => {
+      subscription.unsubscribe();
+    });
+  }
+
+
 }
