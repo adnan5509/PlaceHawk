ab4dddad58149aae986195cc0e822f98c1f8afc3
adnan5509 <adnanafzal5509@gmail.com>
2025-04-23 00:04:20 +0200

    feat: enhance logging interceptor to include response logging for outgoing requests

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpEventType, HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 function loggingInterceptor(request: HttpRequest<unknown>, next: HttpHandlerFn) {
 
     console.log("[Outgoing request]");
     console.log(request)
 
-    return next(request);
+    return next(request).pipe(
+        tap({
+            next: event => {
+                if (event.type === HttpEventType.Response) {
+                    console.log("Incoming Response");
+                    console.log(event.status);
+                    console.log(event.body);
+                }
+            }
+        })
+    );
 }
-
 
 bootstrapApplication(AppComponent, { providers: [provideHttpClient(withInterceptors([loggingInterceptor]))] }).catch((err) => console.error(err));
